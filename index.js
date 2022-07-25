fdfb45a398e98625aff5afeb82a20c00f8b51556
ballhog23 <calebpirkle2@gmail.com>
2022-07-24 21:45:10 -0500

    ttf

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
 console.log(fetch('https://graph.instagram.com/17841400708603821?fields=id,media_url,username,timestamp&access_token=IGQVJXaElvcnRQSzlsNk96ZAF9JYVRKbWhxTjBxVjVfTC1adWhmRE5LTmhVNW04NWNBX1VXS0tKcHBYYXM5U1NpNGQyTEhjU0ZA4WWVZAQndJa0wxVmM5SVFuTE52SDVQSDNmOENSRjhYX3FSYUJGZAFlCNXpsa1JYU3FhNjE0').then((response) => response.json()).then((data) => console.log(data)));
-
